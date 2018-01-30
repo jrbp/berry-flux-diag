corrected sign issues for agreement with abinit (i think they're correct)

pt_pol.py
@@ -183,9 +183,9 @@ def compute_overlap_element(gvs0, ev0_conj, gvs1, ev1, dg):
     this_element = 0.
     for i in range(len(gvs0)):
         for j in range(len(gvs1)):
-            if (gvs0[i][0] == gvs1[j][0] + dg[0]
-               and gvs0[i][1] == gvs1[j][1] + dg[1]
-               and gvs0[i][2] == gvs1[j][2] + dg[2]):
+            if (gvs0[i][0] == gvs1[j][0] - dg[0]
+               and gvs0[i][1] == gvs1[j][1] - dg[1]
+               and gvs0[i][2] == gvs1[j][2] - dg[2]):
                 this_element += ev0_conj[i] * ev1[j]
     return this_element
 
@@ -231,10 +231,10 @@ def bphase_along_string(kpt_string, pt=False):
                 overlap = compute_overlap(kpt_string[i].get_occupied_only(),
                                           kpt_string[i+1].get_occupied_only())
         # this_det = np.linalg.det(overlap)
-        # phase_change = -1 * log(this_det).imag
+        # phase_change = log(this_det).imag
         s, lndet = np.linalg.slogdet(overlap)
         # print(s, lndet)
-        phase_change = -1 * log(s).imag
+        phase_change = log(s).imag
         print(fromkpt.kcoords, "->",
               tokpt.kcoords,
               " ", phase_change / (2 * np.pi))
@@ -254,7 +254,7 @@ def bphase_with_mult(kpt_string):
                                       kpt_string[i+1].get_occupied_only())
         product = np.dot(product, overlap)
     s, lndet = np.linalg.slogdet(product)
-    return -1 * log(s).imag
+    return log(s).imag
 
 
 def det_of_string_mat_mult(kpt_string):
@@ -323,16 +323,16 @@ def get_berry_phase_polarization(wfc, method=None):
             det_strings.append(this_det)
             det_avg += this_det / nstr
 
-            phase0 = -1 * np.arctan2(det_avg.imag, det_avg.real)
-            # phase0 = -1 * np.log(det_avg).imag
+            phase0 = np.arctan2(det_avg.imag, det_avg.real)
+            # phase0 = np.log(det_avg).imag
             det_mod = np.conjugate(det_avg) * det_avg
 
             polb = 0.
             polb_str = []
             for det_string in det_strings:
                 rel_string = (np.conj(det_avg) * det_string) / det_mod
-                dphase = -1 * np.arctan2(rel_string.imag, rel_string.real)
-                # dphase = -1 * np.log(rel_string).imag
+                dphase = np.arctan2(rel_string.imag, rel_string.real)
+                # dphase = np.log(rel_string).imag
                 this_polb = 2*(phase0 + dphase) / (2 * np.pi)
                 polb_str.append(this_polb)
                 # print(this_polb)
@@ -360,6 +360,7 @@ if __name__ == '__main__':
     print("reading {}".format(sys.argv[1]))
     wfc0 = read_wfc(sys.argv[1])
 
+    # bp0 = get_berry_phase_polarization(wfc0, method='verbose')
     bp0 = get_berry_phase_polarization(wfc0)
     print("Electronic berry phase: {}".format(bp0))
 